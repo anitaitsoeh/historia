a495d70b08deb27c48f1b132f921c4b7341262cb
= <=>
2020-10-09 10:26:31 -0500

    Añadí el archivo de main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+class Main (){
+	public static void main(){
+		// Christian
+
+	}
+
+	public static float raizCuadrada(int x){
+		// Ana
+	}
+
+	public static float elevarAlCuadrado(int x){
+		// Marcos
+	}
+
+	public static void ordenarElementos(int x[]){
+		// Joselin
+	}
+}
